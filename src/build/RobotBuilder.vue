d1ef3e35d27e3830fee048bf8f63dd2c2733d5e9
Honayme <honayme@gmail.com>
2018-12-20 14:42:23 +0100

    handle event emitter from child to parent component and add preview on purchased item above add cart button

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -1,20 +1,50 @@
 <template>
   <div class="content">
-    <button class="add-to-cart" @click="addToCart()">Add to Cart</button>
+    <div class="preview">
+      <div class="preview-content">
+        <div class="top-row">
+          <img :src="selectedRobot.head.src"/>
+        </div>
+        <div class="middle-row">
+          <img :src="selectedRobot.leftArm.src" class="rotate-left"/>
+          <img :src="selectedRobot.torso.src"/>
+          <img :src="selectedRobot.rightArm.src" class="rotate-right"/>
+        </div>
+        <div class="bottom-row">
+          <img :src="selectedRobot.base.src"/>
+        </div>
+      </div>
+      <button class="add-to-cart" @click="addToCart()">Add to Cart</button>
+    </div>
     <div class="top-row">
         <!--<div class="robot-name">
           {{selectedRobot.head.title}}
           <span v-if="selectedRobot.head.onSale" class="sale">Sale!</span>
         </div>-->
-        <PartSelector :parts="availableParts.heads" position="top"/>
+        <PartSelector
+          :parts="availableParts.heads"
+          position="top"
+          @partSelected="part => selectedRobot.head=part"/>
     </div>
     <div class="middle-row">
-      <PartSelector :parts="availableParts.arms" position="left"/>
-      <PartSelector :parts="availableParts.torsos" position="center"/>
-      <PartSelector :parts="availableParts.arms" position="right"/>
+      <PartSelector
+        :parts="availableParts.arms"
+        position="left"
+        @partSelected="part => selectedRobot.leftArm=part"/>
+      <PartSelector
+        :parts="availableParts.torsos"
+        position="center"
+        @partSelected="part => selectedRobot.torso=part"/>
+      <PartSelector
+        :parts="availableParts.arms"
+        position="right"
+        @partSelected="part => selectedRobot.rightArm=part"/>
     </div>
     <div class="bottom-row">
-      <PartSelector :parts="availableParts.bases" position="bottom"/>
+      <PartSelector
+        :parts="availableParts.bases"
+        position="bottom"
+        @partSelected="part => selectedRobot.base=part"/>
     </div>
     <div>
       <h1>Cart</h1>
@@ -28,7 +58,7 @@
         <tbody>
         <tr v-for="(robot, index) in cart" :key="index">
           <td>{{robot.head.title}}</td>
-          <td class="cost">{{robot.cost}}</td>
+          <td class="cost">{{robot.head.cost}}</td>
         </tr>
         </tbody>
       </table>
@@ -59,7 +89,7 @@ export default{
   },
   mixins: [createdHookMixin],
   computed: {
-    saleBorderClass(){
+    saleBorderClass() {
       return this.selectedRobot.head.onSale
         ? 'sale-border'
         : '';
@@ -80,9 +110,9 @@ export default{
         + robot.leftArm.cost
         + robot.torso.cost
         + robot.rightArm.cost
-        + robot.bases.cost;
+        + robot.base.cost;
       this.cart.push(Object.assign({}, robot, { cost }));
-      console.log(this.cart);
+      console.log(robot.head);
     },
 
   },
@@ -194,8 +224,7 @@ export default{
   }
   .add-to-cart{
     position: absolute;
-    right: 30px;
-    width: 220px;
+    width: 210px;
     padding: 3px;
     font-size: 16px;
   }
@@ -209,4 +238,26 @@ export default{
   .sale-border {
     border: 3px solid red;
   }
+  .preview {
+    position: absolute;
+    top: -20px;
+    right: 0;
+    width: 210px;
+    height: 210px;
+    padding: 5px;
+  }
+  .preview-content {
+    border: 1px solid #999;
+  }
+  .preview img {
+    width: 50px;
+    height: 50px;
+  }
+  .rotate-right {
+    transform: rotate(90deg);
+  }
+  .rotate-left {
+    transform: rotate(-90deg);
+  }
+
 </style>
